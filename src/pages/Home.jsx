64dc65406e05d9479fd9a5fd47bdacea53bd5f57
Faruk Erge <[farukerge98@gmail.com]>
2022-02-23 22:11:06 +0300

    home page file created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar'
 function Home() {
   return (
       <div className=' p-12 flex justify-between items-center'>
-          <h1>logo</h1>
+          <h1>logo area</h1>
           <Navbar />
       </div>
   )
